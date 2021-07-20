bf6a8b3a8eb94ea0105b7b667cc3ba9359d8b18c
tod-easton <tod.easton.fv6o@statefarm.com>
2021-07-20 09:00:26 -0700

    add calculate logic

--- src/main/java/org/easton/SumOfPositivesCLI.java
+++ src/main/java/org/easton/SumOfPositivesCLI.java
@@ -2,6 +2,20 @@ package org.easton;
 
 public class SumOfPositivesCLI {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+
+        if (args.length == 0) {
+            System.out.println("0");
+            System.exit(1);
+        }
+        int calSum = 0;
+        for (int i = 0; i < args.length; i++) {
+            int curInd = Integer.parseInt(args[i]);
+            if (curInd > 0) {
+                calSum += curInd;
+            }
+        }
+
+        System.out.println(calSum);
     }
 }
